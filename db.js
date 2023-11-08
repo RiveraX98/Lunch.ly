4cf45805399654547f3efa94cc046584c346b895
Xavier Rivera <xrivera@Xaviers-MacBook-Air.local>
2023-11-08 01:17:24 -0500

    DB_URI env variable added

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 /** Database for lunchly */
 
 const pg = require("pg");
-const DB_URI = "postgresql:///lunchly";
+const DB_URI = process.env.NODE_ENV || "postgresql:///lunchly";
 const db = new pg.Client(DB_URI);
 
 db.connect();
